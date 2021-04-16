578ac4de532dcbd17e23e212fab286d3bc29218a
ᑭᖇᗴᔕᕼ ᗝᑎƳᗴᗴ <presh_onyee@outlook.com>
2021-04-16 01:41:42 -0700

    fix(radar): update radar to fetch entries from remote server

--- client/src/components/Aside.tsx
+++ client/src/components/Aside.tsx
@@ -2,6 +2,11 @@ import styled from "styled-components";
 import { Button, Avatar } from "antd";
 import { PlusOutlined } from "@ant-design/icons";
 import UserCard from "./UserCard";
+import { useEffect, useState } from "react";
+import { BASE_URL } from "../constants/BaseURL";
+import axios, { AxiosResponse } from "axios";
+import { TOKEN } from "../constants/Token";
+import { ExternalLink } from "react-feather";
 
 const Radar = styled.div`
   width: 100%;
@@ -37,7 +42,32 @@ const Wrapper = styled.div`
   }
 `;
 
+interface IEntries {
+  _id: string;
+  avatar: string;
+  companyName: string;
+  linkToCareersPage: string;
+}
+
 const Aside = () => {
+  const [entries, setEntries] = useState<IEntries[]>([]);
+
+  const getRadarEntries = () => {
+    axios
+      .get(`${BASE_URL}/radar`, {
+        headers: {
+          Authorization: `Bearer ${TOKEN}`,
+        },
+      })
+      .then((response: AxiosResponse<any>) => {
+        setEntries(response.data.result);
+      });
+  };
+
+  useEffect(() => {
+    getRadarEntries();
+  }, []);
+
   return (
     <Wrapper>
       <UserCard />
@@ -48,34 +78,18 @@ const Aside = () => {
             Add company
           </Button>
         </div>
-        <div className="list-item">
-          <Avatar size={48} />
-          <div className="company">
-            <h3>Company Name</h3>
-            <a href="https://">See openings</a>
+        {entries.map((entry) => (
+          <div key={entry._id} className="list-item">
+            <Avatar size={48} src={entry.avatar} />
+            <div className="company">
+              <h3>{entry.companyName}</h3>
+              <span>
+                <a href={entry.linkToCareersPage}>See openings</a>{" "}
+                <ExternalLink color="#ff5a5f" size={14} />
+              </span>
+            </div>
           </div>
-        </div>
-        <div className="list-item">
-          <Avatar size={48} />
-          <div className="company">
-            <h3>Company Name</h3>
-            <a href="https://">See openings</a>
-          </div>
-        </div>
-        <div className="list-item">
-          <Avatar size={48} />
-          <div className="company">
-            <h3>Company Name</h3>
-            <a href="https://">See openings</a>
-          </div>
-        </div>
-        <div className="list-item">
-          <Avatar size={48} />
-          <div className="company">
-            <h3>Company Name</h3>
-            <a href="https://">See openings</a>
-          </div>
-        </div>
+        ))}
       </Radar>
     </Wrapper>
   );
