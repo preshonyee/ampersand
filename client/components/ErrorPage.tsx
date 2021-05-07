683102b7a306b9f61d62ea64fcb36aa7a36f8f68
ᑭᖇᗴᔕᕼ ᗝᑎƳᗴᗴ <presh_onyee@outlook.com>
2021-05-07 01:27:11 -0700

    refactor(nextjs): remove old files

--- client/components/ErrorPage.tsx
+++
@@ -1,9 +0,0 @@
-const ErrorPage = () => {
-  return (
-    <div>
-      <h1>This page does not exist</h1>
-    </div>
-  );
-};
-
-export default ErrorPage;
--- client/constants/Token.ts
+++
@@ -1,1 +0,0 @@
-export const TOKEN = localStorage.getItem("jwt");
